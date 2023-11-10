e1c2dfff7015a4e97f15e92d95011a1650366455
sur-yang <xiaobaoy@mail.ccmu.edu.cn>
2023-11-10 15:57:31 +0800

    Initial commit

---
+++ R/LogisticSPSSlike.R
@@ -0,0 +1,29 @@
+#' To keep the variable names in accordance with SPSS output when performing logistic regression using R
+#'
+#' @param fit an object of class inheriting from "glm".
+#'
+#' @return a data frame contains B, Wald, OR, CI, and P values.
+#' @export
+#'
+#' @examples
+#' if (!require("AER")) install.packages("AER")
+#' library("AER")
+#' data(Affairs)
+#' Affairs[Affairs$affairs>1,]$affairs<-1
+#' fit<-glm(affairs~.,data=Affairs,family=binomial())
+#' formatFit(fit)
+#'
+LogisticSPSSlike<-function(fit){
+  p<-summary(fit)$coefficients[,4]
+  wald<-summary(fit)$coefficients[,3]^2
+  valueB<-coef(fit)
+  valueOR<-exp(coef(fit))
+  confitOR<-exp(confint(fit))
+  data.frame(
+    B=round(valueB,3),
+    Wald=round(wald,3),
+    OR_with_CI=paste(round(valueOR,3),"(",
+                     round(confitOR[,1],3),"~",round(confitOR[,2],3),")",sep=""),
+    P=format.pval(p,digits = 3,eps=0.001)
+  )
+}
